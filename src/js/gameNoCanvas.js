7b390bf08b340d4601ed9dfb1abd8d57e6dcd84d
KrtNinja <kitonashi@yandex.ru>
2020-01-31 18:28:31 +0300

    rebuild visual

--- src/js/gameNoCanvas.js
+++ src/js/gameNoCanvas.js
@@ -4,10 +4,10 @@ document.body.onload = addElement;
   function addElement() {
 
     var newDiv = document.createElement("div");
-        newDiv.innerHTML = "<h1>Привет!</h1>";
+        newDiv.innerHTML = "<h1 style='color: yellow;'>2</h1>";
 
     // добавляем только что созданый элемент в дерево DOM
 
-    my_div = document.getElementById("gameArea");
-    document.body.innerHTML(newDiv, my_div);
+    my_div = document.getElementById("gameZone");
+    document.body.prepend(newDiv, my_div);
   }
